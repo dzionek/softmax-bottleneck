6c258169f6869978872cf881ae421f89b9b5a500
Bartosz Dzionek <vburtoch@gmail.com>
2023-02-08 03:54:23 +0000

    Fix add experiments 2

--- cluster/run_experiments.sh
+++ cluster/run_experiments.sh
@@ -6,7 +6,7 @@ for model in "${MODELS[@]}"
 do
   for d in "${DS[@]}"
   do
-    printf "#!/bin/bash\n/usr/bin/python ${model}_${d} mnist $model -c cuda -e 30 -d $d" >> ${model}_${d}.sh
+    printf "#!/bin/bash\n/usr/bin/python main.py ${model}_${d} mnist $model -c cuda -e 30 -d $d" >> ${model}_${d}.sh
     sbatch --time=08:00:00 --gres=gpu:1 ${model}_${d}.sh
     rm -rf ${model}_${d}.sh
   done
