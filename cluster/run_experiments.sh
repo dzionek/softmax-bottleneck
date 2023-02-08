f65d395f3d497c7b7cc486fb544609f727bd1993
Bartosz Dzionek <vburtoch@gmail.com>
2023-02-08 03:37:27 +0000

    Prepare for experiments

---
+++ cluster/run_experiments.sh
@@ -0,0 +1,14 @@
+##!/bin/bash
+MODELS=('softmax' 'sigsoftmax' 'plif')
+DS=('128', '5', '3')
+
+for model in "${MODELS[@]}"
+do
+  for d in "${DS[@]}"
+  do
+    echo "#!/bin/bash\n/usr/bin/python ${model}_${d} mnist $model -c cuda -e 30 -d $d" >> ${model}_${d}.sh
+    sbatch --time=08:00:00 --gres=gpu:1 ${model}_${d}.sh
+    rm -rf ${model}_${d}.sh
+  done
+done
+
--- cluster/training.py
+++ cluster/training.py
@@ -11,54 +11,57 @@ def train(args, model, device, train_loader, optimizer, epoch, stats):
     start = time.time()
 
     model.train()
-    train_loss = 0
-    correct = 0
     for batch_idx, (data, target) in enumerate(train_loader):
         data, target = data.to(device), target.to(device)
         optimizer.zero_grad()
         output = model(data)
         loss = F.nll_loss(output, target)
-        train_loss += F.nll_loss(output, target, reduction='sum').item()
-        pred = output.argmax(dim=1, keepdim=True)
-        correct += pred.eq(target.view_as(pred)).sum().item()
         loss.backward()
         optimizer.step()
 
-    train_loss /= len(train_loader.dataset)
-
-    stats['train_loss'].append(round(train_loss, 4))
-    stats['train_accuracy'].append(round(100.*correct / len(train_loader.dataset), 1))
-    stats['log'].append(
-        f'Training set: Average loss: {stats["train_loss"][-1]}, '
-        f'Accuracy: {correct}/{len(train_loader.dataset)} ({stats["train_accuracy"][-1]}%)')
-    print(stats['log'][-1])
-
     end = time.time()
     stats['time'].append(round(end-start))
-    stats['log'].append(f'Epoch took {stats["time"][-1]}s.')
+    stats['log'].append(f'Epoch training took {stats["time"][-1]}s.')
     print(stats['log'][-1])
 
 
-def test(model, device, test_loader, stats):
+def test(model, device, train_loader, test_loader, stats):
     model.eval()
+    train_loss = 0
+    train_correct = 0
     test_loss = 0
-    correct = 0
+    test_correct = 0
     with torch.no_grad():
+        for data, target in train_loader:
+            data, target = data.to(device), target.to(device)
+            output = model(data)
+            train_loss += F.nll_loss(output, target, reduction='sum').item()
+            pred = output.argmax(dim=1, keepdim=True)
+            train_correct += pred.eq(target.view_as(pred)).sum().item()
+
         for data, target in test_loader:
             data, target = data.to(device), target.to(device)
             output = model(data)
             batch_loss = F.nll_loss(output, target, reduction='sum').item()
             test_loss += batch_loss
             pred = output.argmax(dim=1, keepdim=True)
-            correct += pred.eq(target.view_as(pred)).sum().item()
+            test_correct += pred.eq(target.view_as(pred)).sum().item()
 
+    train_loss /= len(train_loader.dataset)
     test_loss /= len(test_loader.dataset)
 
+    stats['train_loss'].append(round(train_loss, 4))
+    stats['train_accuracy'].append(round(100.*train_correct / len(train_loader.dataset), 1))
+    stats['log'].append(
+        f'Training set: Average loss: {stats["train_loss"][-1]}, '
+        f'Accuracy: {train_correct}/{len(train_loader.dataset)} ({stats["train_accuracy"][-1]}%)')
+    print(stats['log'][-1])
+
     stats['test_loss'].append(round(test_loss, 4))
-    stats['test_accuracy'].append(round(100.*correct / len(test_loader.dataset), 1))
+    stats['test_accuracy'].append(round(100.*test_correct / len(test_loader.dataset), 1))
     stats['log'].append(
         f'Test set: Average loss: {stats["test_loss"][-1]}, '
-        f'Accuracy: {correct}/{len(test_loader.dataset)} ({stats["test_accuracy"][-1]}%)')
+        f'Accuracy: {test_correct}/{len(test_loader.dataset)} ({stats["test_accuracy"][-1]}%)')
     print(stats['log'][-1])
 
 
@@ -83,7 +86,7 @@ def run_experiment(args, network, dataset1, dataset2):
     scheduler = StepLR(optimizer, step_size=1, gamma=args.gamma)
     for epoch in range(1, args.epochs + 1):
         train(args, model, device, train_loader, optimizer, epoch, stats)
-        test(model, device, test_loader, stats)
+        test(model, device, train_loader, test_loader, stats)
         scheduler.step()
 
     stats['log'].append(f'Done! Total time {round(sum(stats["time"])/60)} min.')
