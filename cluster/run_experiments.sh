c9c42eabc96623f495ba3eef07b4b31c35b6991e
Bartosz Dzionek <vburtoch@gmail.com>
2023-03-08 20:16:01 +0100

    Test if file exists

--- cluster/run_experiments.sh
+++ cluster/run_experiments.sh
@@ -7,9 +7,15 @@ for model in "${MODELS[@]}"
 do
   for d in "${DS[@]}"
   do
-    printf "#!/bin/bash\n/usr/bin/python main.py ${model}_${d} mnist $model -c cuda -e 30 -d $d" >> ${model}_${d}.sh
-    sbatch --time=08:00:00 --gres=gpu:1 ${model}_${d}.sh
-    rm -rf ${model}_${d}.sh
+    FILE=/etc/resolv.conf
+    if test -f "experiments/${model}_${d}"; then
+      echo "Skipping. $FILE already exists."
+    else
+      echo "$FILE does not exist"
+#      printf "#!/bin/bash\n/usr/bin/python main.py ${model}_${d} mnist $model -c cuda -e 30 -d $d" >> ${model}_${d}.sh
+#      sbatch --time=08:00:00 --gres=gpu:1 ${model}_${d}.sh
+#      rm -rf ${model}_${d}.sh
+    fi
   done
 done
 
