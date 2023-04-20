def1118f58c3bc8e6ef2fe8634bc7c843de5c4e2
Bartosz Dzionek <vburtoch@gmail.com>
2023-04-20 20:32:03 +0200

    Make LSE plots bigger

--- figures/log_sum_exp.py
+++ figures/log_sum_exp.py
@@ -2,6 +2,15 @@ import matplotlib.pyplot as plt
 from matplotlib import cm
 import numpy as np
 
+BIGGER_SIZE = 15
+
+plt.rc('font', size=BIGGER_SIZE)          # controls default text sizes
+plt.rc('axes', titlesize=BIGGER_SIZE)     # fontsize of the axes title
+plt.rc('axes', labelsize=BIGGER_SIZE)    # fontsize of the x and y labels
+plt.rc('xtick', labelsize=BIGGER_SIZE)    # fontsize of the tick labels
+plt.rc('ytick', labelsize=BIGGER_SIZE)    # fontsize of the tick labels
+plt.rc('legend', fontsize=BIGGER_SIZE)    # legend fontsize
+plt.rc('figure', titlesize=BIGGER_SIZE)
 _, ax = plt.subplots()
 
 X = np.arange(0, 10, 0.1)
@@ -10,11 +19,12 @@ X, Y = np.meshgrid(X, Y)
 Z = np.logaddexp(X, Y)
 
 contour = ax.contour(X, Y, Z, cmap=cm.coolwarm)
-ax.clabel(contour, inline=True, fontsize=10)
+ax.clabel(contour, inline=True, fontsize=BIGGER_SIZE)
 
 plt.xlabel("x")
 plt.ylabel("y")
 plt.tight_layout()
+
 
 # plt.show()
 plt.savefig('lse_contour.pdf')
